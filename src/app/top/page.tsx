2a7411c1462b26e31305f030740731238b4cd96f
Makochaaan <arowana326.315@gmail.com>
2024-06-16 17:13:43 +0900

    [add]Works in Top Page

--- src/app/top/page.tsx
+++ src/app/top/page.tsx
@@ -30,35 +30,34 @@ return (
                 </div>
             </div>
 
-            <div className="mt-80 z-[-1] flex-col place-items-center min-h-screen before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-                <h1>Work</h1>
-                <div className="grid flex text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-                <a
-                    href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2 className="mb-3 text-2xl font-semibold">
-                    Docs{" "}
-                    <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                        -&gt;
-                    </span>
-                    </h2>
-                </a>
-                <a
-                    href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2 className="mb-3 text-2xl font-semibold">
-                    Docs{" "}
-                    <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                        -&gt;
-                    </span>
-                    </h2>
-                </a>
+            <div className="mt-60 flex-col min-h-screen">
+                <h1 className="mb-10">Work</h1>
+                <div className="grid text-center lg:mb-10 lg:w-full lg:max-w-5xl lg:grid-cols-2 lg:text-left lg:gap-10">
+                    <div className="relative group">
+                        <a href="https://yorimichizu.com" target="_blank" rel="noopener noreferrer">
+                            <img src="/yorimichizu.png" className="rounded-md border-2 border-indigo-600" />
+                            <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                                <div className="absolute bottom-0 right-0 bg-indigo-500/70 flex-col px-16 py-2 rounded-tl-lg rounded-br-lg">
+                                    <p>よりみちず</p>
+                                    <p>2023/10</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="relative group">
+                        <a href="https://unityroom.com/games/my_new_gear" target="_blank" rel="noopener noreferrer">
+                            <img src="/mynewgear.png" className="rounded-md border-2 border-indigo-600" />
+                            <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                                <div className="absolute bottom-0 right-0 bg-indigo-500/70 flex-col px-16 py-2 rounded-tl-lg rounded-br-lg">
+                                    <p>よりみちず</p>
+                                    <p>2023/10</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div className="flex justify-center">
+                    <NextLink href="../work" className="bg-indigo-600 text-white rounded-md px-4 py-2">もっと見る→</NextLink>
                 </div>
             </div>
         </main>

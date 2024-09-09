b94080474fbdcd7c6e7e74f582de98469e02ac4a
Makochaaan <arowana326.315@gmail.com>
2024-09-09 18:41:59 +0900

    [fix]fix

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className="py-10 px-80 w-full flex items-center justify-between text-blue-800 border-t border-blue-400">
             <div className="flex-col items-center justify-center">
-                <p>2024 {process.env.NEXT_PUBLIC_NEXT_PUBLIC_NAME}.</p>
+                <p>2024 {process.env.NEXT_PUBLIC_NEXT_PUBLIC_NAME}..</p>
                 <p>Made with React, Next.js</p>
             </div>
         </footer>
